6c1f2d09bb043ab2b8214898e24cdaf91662e111
Sean Tichenor <jtart2133014@gmail.com>
2025-05-20 21:34:58 -0400

    Update USTAF_ADL_Tokenizer.h

--- USTAF_ADL_Tokenizer.h
+++ USTAF_ADL_Tokenizer.h
@@ -1,32 +1,32 @@
-#pragma once
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <chrono>
+#ifndef USTAF_ADL_TOKENIZER_H
+#define USTAF_ADL_TOKENIZER_H
 
-enum AccessTier {
-    PUBLIC = 0,
-    PRIVATE = 1,
-    FOREIGN = 2
-};
+#include <string>
+#include <map>
+#include <chrono>
+#include <set>
+
+enum AccessTier { PUBLIC, PRIVATE, FOREIGN };
 
 struct TokenData {
     AccessTier tier;
-    std::chrono::system_clock::time_point expiry;
+    std::chrono::time_point<std::chrono::system_clock> expiry;
     std::string dual_key;
     std::string quantum_hash;
 };
 
 class USTAF_ADL_Tokenizer {
 private:
-    std::unordered_map<std::string, TokenData> registry;
-    std::unordered_set<std::string> revocation_list;
+    std::map<std::string, TokenData> registry;
+    std::set<std::string> revocation_list;
 
 public:
-    void register_token(const std::string& id, AccessTier tier, int lifespan_seconds, const std::string& dual_key);
+    void register_token(const std::string& id, AccessTier tier, int lifespan_seconds, const std::string& dual_key = "");
+    AccessTier get_access_tier(const std::string& id, const std::string& secondary_key = "");
+    void audit_view_request(const std::string& requester, const std::string& resource);
     bool is_expired(const std::string& id);
     void revoke_token(const std::string& id);
     bool verify_token_hash(const std::string& id, const std::string& input_hash);
-    AccessTier get_access_tier(const std::string& id, const std::string& secondary_key = "");
-    void audit_view_request(const std::string& requester, const std::string& resource);
 };
+
+#endif // USTAF_ADL_TOKENIZER_H
