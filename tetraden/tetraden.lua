bdc324beab16e95d697cc7316b8d3cdd64bc77d3
Sean Tichenor <jtart2133014@gmail.com>
2025-05-20 22:09:15 -0400

    Update tetraden.lua

--- tetraden/tetraden.lua
+++ tetraden/tetraden.lua
@@ -1,9 +1,20 @@
 -- TETRADEN :: Post-Quantum Lua Accelerator [BFE Edition]
--- Format: Lua (for Termux runtime)
+-- Format: Lua (LuaJIT for Termux runtime)
 
 local ffi = require("ffi")
-local openssl = require("openssl")
-local sha256 = openssl.digest.get("sha256")
+
+-- OpenSSL SHA256 via FFI
+ffi.cdef[[
+    void SHA256(const void *d, size_t n, unsigned char *md);
+]]
+
+local function sha256_hex(input)
+    local hash = ffi.new("unsigned char[32]")
+    ffi.C.SHA256(input, #input, hash)
+    return (string.gsub(ffi.string(hash, 32), ".", function(c)
+        return string.format("%02X", string.byte(c))
+    end))
+end
 
 -- Layer I: Entropy Wave Sampler
 function wave_entropy()
@@ -15,26 +26,28 @@ end
 
 -- Layer II: AQVP-inspired Lattice Modifier
 function wave_lattice_mod(entropy)
-    local mod = ""
+    local mod = {}
     for i = 1, #entropy do
         local byte = entropy:byte(i)
-        mod = mod .. string.char((byte * (i % 13 + 1)) % 256)
+        table.insert(mod, string.char((byte * (i % 13 + 1)) % 256))
     end
-    return mod
+    return table.concat(mod)
+end
+
+-- Duoplex Interplex Hashing (Telemetry Mix)
+function duoplex_interplex(str)
+    local mix = {}
+    for i = 1, #str do
+        local c = str:byte(i)
+        table.insert(mix, ((c * (i % 17 + 3)) ~ (255 - i)) % 256)
+    end
+    return sha256_hex(string.char(table.unpack(mix)))
 end
 
 -- Layer III: BFE-Quantum Output Cycler
 function wave_output_cycler(data, id_tag, op_tag)
-    local digest = sha256()
-    digest:update(data .. id_tag .. op_tag)
-    local hash = digest:final()
-
-    -- QQUAp Encoded (Hexified for seal)
-    local seal = ""
-    for i = 1, #hash do
-        seal = seal .. string.format("%02X", hash:byte(i))
-    end
-
+    local fused = data .. duoplex_interplex(id_tag) .. duoplex_interplex(op_tag)
+    local seal = sha256_hex(fused)
     return string.format("BFE::%s::%s::%s", id_tag, op_tag, seal)
 end
 
