17b80cccbfada2b3c65c813321cb190214494b2c
Sean Tichenor <jtart2133014@gmail.com>
2025-05-20 22:17:43 -0400

    Update tetraden.lua

--- tetraden/tetraden.lua
+++ tetraden/tetraden.lua
@@ -1,9 +1,11 @@
 -- TETRADEN :: Post-Quantum Lua Accelerator [BFE Edition]
 -- Format: LuaJIT for Termux
 
+local unpack = table.unpack or unpack
 local bit = require("bit")
-
 local ffi = require("ffi")
+local C = ffi.load("crypto")  -- Load OpenSSL libcrypto
+
 ffi.cdef[[
     void SHA256(const void *d, size_t n, unsigned char *md);
 ]]
@@ -11,14 +13,14 @@ ffi.cdef[[
 -- Native SHA-256 hashing via OpenSSL
 local function sha256_hex(input)
     local hash = ffi.new("unsigned char[32]")
-    ffi.C.SHA256(input, #input, hash)
+    C.SHA256(input, #input, hash)
     return (string.gsub(ffi.string(hash, 32), ".", function(c)
         return string.format("%02X", string.byte(c))
     end))
 end
 
 -- Layer I: Entropy Wave Sampler
-function wave_entropy()
+local function wave_entropy()
     local f = io.open("/dev/urandom", "rb")
     local entropy = f:read(128)
     f:close()
@@ -26,7 +28,7 @@ function wave_entropy()
 end
 
 -- Layer II: AQVP-inspired Lattice Modifier
-function wave_lattice_mod(entropy)
+local function wave_lattice_mod(entropy)
     local mod = {}
     for i = 1, #entropy do
         local byte = entropy:byte(i)
@@ -36,7 +38,7 @@ function wave_lattice_mod(entropy)
 end
 
 -- Duoplex Interplex Hashing Layer
-function duoplex_interplex(str)
+local function duoplex_interplex(str)
     local mix = {}
     for i = 1, #str do
         local c = str:byte(i)
@@ -47,7 +49,7 @@ function duoplex_interplex(str)
 end
 
 -- Layer III: BFE-Quantum Output Cycler
-function wave_output_cycler(data, id_tag, op_tag)
+local function wave_output_cycler(data, id_tag, op_tag)
     local fused = data .. duoplex_interplex(id_tag) .. duoplex_interplex(op_tag)
     local seal = sha256_hex(fused)
     return string.format("BFE::%s::%s::%s", id_tag, op_tag, seal)
